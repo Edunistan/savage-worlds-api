ea4154539c90af01d7f4825836f267d99fe649cc
Eduuuuuuuuuuuuuuuuuuuuu <eduardo.soto@uc.cl>
2026-03-06 12:58:44 -0300

    File tree fixed

--- src/index.js
+++ src/index.js
@@ -4,19 +4,55 @@ const express = require("express");
 const pool = require("./db/pool");
 
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Savage Worlds API running");
 });
 
-app.get("/test-db", async (req, res) => {
+app.get("/characters", async (req, res) => {
+  const result = await pool.query("SELECT * FROM characters");
+  res.json(result.rows);
+});
+
+app.post("/characters", async (req, res) => {
   try {
-    const result = await pool.query("SELECT NOW()");
-    res.json(result.rows);
+    const {
+      name,
+      ancestry_id,
+      bennies,
+      size,
+      wild_die,
+      wounds_level,
+      fatigue_level,
+      power_points,
+      rank
+    } = req.body;
+
+    const result = await pool.query(
+      `INSERT INTO characters
+      (name, ancestry_id, bennies, size, wild_die, wounds_level, fatigue_level, power_points, rank)
+      VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)
+      RETURNING *`,
+      [
+        name,
+        ancestry_id,
+        bennies,
+        size,
+        wild_die,
+        wounds_level,
+        fatigue_level,
+        power_points,
+        rank
+      ]
+    );
+
+    res.status(201).json(result.rows[0]);
+
   } catch (err) {
     console.error(err);
-    res.status(500).send("DB error");
+    res.status(500).json({ error: "Error creando personaje" });
   }
 });
 
